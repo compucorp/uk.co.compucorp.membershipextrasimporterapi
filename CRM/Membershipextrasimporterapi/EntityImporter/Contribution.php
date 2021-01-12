f3c1d8598c2d4097dd8cf88e04f5a33883d7ed25
Omar abu hussein <opetmar91@gmail.com>
2021-01-12 11:53:07 +0000

    MAE-434: Add contribution importer

---
+++ CRM/Membershipextrasimporterapi/EntityImporter/Contribution.php
@@ -0,0 +1,243 @@
+<?php
+
+class CRM_Membershipextrasimporterapi_EntityImporter_Contribution {
+
+  private $rowData;
+
+  private $contactId;
+
+  private $recurContributionId;
+
+  private $cachedValues;
+
+  public function __construct($rowData, $contactId, $recurContributionId) {
+    $this->rowData = $rowData;
+    $this->contactId = $contactId;
+    $this->recurContributionId = $recurContributionId;
+  }
+
+  public function import() {
+    if (empty($this->rowData['contribution_external_id'])) {
+      return NULL;
+    }
+
+    $contributionId = $this->getContributionIdIfExist();
+    if ($contributionId) {
+      return $contributionId;
+    }
+
+    $sqlParams = $this->prepareSqlParams();
+    $sqlQuery = "INSERT INTO `civicrm_contribution` (`contact_id` , `financial_type_id` , `payment_instrument_id` , 
+                 `receive_date` , `total_amount` , `currency`, `contribution_recur_id` , `is_pay_later`,
+                  `contribution_status_id`) 
+                 VALUES (%1, %2, %3, %4, %5, %6, %7, %8, %9)";
+    CRM_Core_DAO::executeQuery($sqlQuery, $sqlParams);
+
+    $dao = CRM_Core_DAO::executeQuery('SELECT LAST_INSERT_ID() as contribution_id');
+    $dao->fetch();
+    $contributionId = $dao->contribution_id;
+
+    $sqlQuery = "INSERT INTO `civicrm_value_contribution_ext_id` (`entity_id` , `external_id`) 
+           VALUES ({$contributionId}, %1)";
+    CRM_Core_DAO::executeQuery($sqlQuery, [1 => [$this->rowData['contribution_external_id'], 'String']]);
+
+    $mappedContributionParams = $this->mapContributionSQLParamsToNames($sqlParams);
+    $financialTransactionId = $this->createFinancialTransactionRecord($mappedContributionParams);
+    $this->createEntityFinancialTransactionRecord($contributionId, $financialTransactionId, $mappedContributionParams);
+
+    return $contributionId;
+  }
+
+  private function getContributionIdIfExist() {
+    $sqlQuery = "SELECT entity_id as id FROM civicrm_value_contribution_ext_id WHERE external_id = %1";
+    $contributionId = CRM_Core_DAO::executeQuery($sqlQuery, [1 => [$this->rowData['contribution_external_id'], 'String']]);
+    $contributionId->fetch();
+
+    if (!empty($contributionId->id)) {
+      return $contributionId->id;
+    }
+
+    return NULL;
+  }
+
+  private function prepareSqlParams() {
+    $financialTypeId = $this->getFinancialTypeId();
+    $paymentMethodId = $this->getPaymentMethodId();
+    $receiveDate = $this->formatRowDate('contribution_received_date');
+    $totalAmount = $this->getTotalAmount();
+    $currency = $this->getCurrency();
+    $isPayLater = $this->calculateIsPayLaterFlag();
+    $contributionStatusId  = $this->getContributionStatusId();
+
+    return [
+      1 => [$this->contactId, 'Integer'],
+      2 => [$financialTypeId, 'Integer'],
+      3 => [$paymentMethodId, 'Integer'],
+      4 => [$receiveDate, 'Date'],
+      5 => [$totalAmount, 'Money'],
+      6 => [$currency, 'String'],
+      7 => [$this->recurContributionId, 'Integer'],
+      8 => [$isPayLater, 'Integer'],
+      9 => [$contributionStatusId, 'Integer'],
+    ];
+  }
+
+  private function mapContributionSQLParamsToNames($contributionSqlParams) {
+    return [
+      'contact_id' => $contributionSqlParams[1][0],
+      'financial_type_id' => $contributionSqlParams[2][0],
+      'payment_method_id' => $contributionSqlParams[3][0],
+      'receive_date' => $contributionSqlParams[4][0],
+      'total_amount' => $contributionSqlParams[5][0],
+      'currency' => $contributionSqlParams[6][0],
+      'recur_contribution_id' => $contributionSqlParams[7][0],
+      'is_pay_later' => $contributionSqlParams[8][0],
+      'contribution_status_id' => $contributionSqlParams[9][0],
+    ];
+  }
+
+  private function getFinancialTypeId() {
+    if (!isset($this->cachedValues['financial_types'])) {
+      $sqlQuery = "SELECT id, name FROM civicrm_financial_type";
+      $result = CRM_Core_DAO::executeQuery($sqlQuery);
+      while ($result->fetch()) {
+        $this->cachedValues['financial_types'][$result->name] = $result->id;
+      }
+    }
+
+    if (!empty($this->cachedValues['financial_types'][$this->rowData['contribution_financial_type']])) {
+      return $this->cachedValues['financial_types'][$this->rowData['contribution_financial_type']];
+    }
+
+    throw new CRM_Membershipextrasimporterapi_Exception_InvalidContributionFieldException('Invalid contribution "Financial Type"', 100);
+  }
+
+  private function getPaymentMethodId() {
+    if (!isset($this->cachedValues['payment_methods'])) {
+      $sqlQuery = "SELECT cov.name as name, cov.value as id FROM civicrm_option_value cov 
+                  INNER JOIN civicrm_option_group cog ON cov.option_group_id = cog.id 
+                  WHERE cog.name = 'payment_instrument'";
+      $result = CRM_Core_DAO::executeQuery($sqlQuery);
+      while ($result->fetch()) {
+        $this->cachedValues['payment_methods'][$result->name] = $result->id;
+      }
+    }
+
+    if (!empty($this->cachedValues['payment_methods'][$this->rowData['contribution_payment_method']])) {
+      return $this->cachedValues['payment_methods'][$this->rowData['contribution_payment_method']];
+    }
+
+    throw new CRM_Membershipextrasimporterapi_Exception_InvalidContributionFieldException('Invalid contribution "Payment Method"', 200);
+  }
+
+  private function formatRowDate($dateColumnName) {
+    if (!empty($this->rowData[$dateColumnName])) {
+      $date = DateTime::createFromFormat('YmdHis', $this->rowData[$dateColumnName]);
+      $date = $date->format('YmdHis');
+    }
+    else {
+      $date = new DateTime();
+      $date = $date->format('YmdHis');
+    }
+
+    return $date;
+  }
+
+  private function getTotalAmount() {
+    //todo : figure out a way to calculate amount, defaulting it to zero for now
+    return 0;
+  }
+
+  private function getCurrency() {
+    // todo : not in mapping document, need to discuss with others how to get it.
+    return 'GBP';
+  }
+
+  private function calculateIsPayLaterFlag() {
+    $statusName = 'Completed';
+    if (!empty($this->rowData['contribution_status'])) {
+      $statusName = $this->rowData['contribution_status'];
+    }
+
+    if (in_array($statusName, ['Pending', 'In Progress'])) {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  private function getContributionStatusId() {
+    $statusName = 'Completed';
+    if (!empty($this->rowData['contribution_status'])) {
+      $statusName = $this->rowData['contribution_status'];
+    }
+
+    if (!isset($this->cachedValues['contribution_statuses'])) {
+      $sqlQuery = "SELECT cov.name as name, cov.value as id FROM civicrm_option_value cov 
+                  INNER JOIN civicrm_option_group cog ON cov.option_group_id = cog.id 
+                  WHERE cog.name = 'contribution_status'";
+      $result = CRM_Core_DAO::executeQuery($sqlQuery);
+      while ($result->fetch()) {
+        $this->cachedValues['contribution_statuses'][$result->name] = $result->id;
+      }
+    }
+
+    if (!empty($this->cachedValues['contribution_statuses'][$statusName])) {
+      return $this->cachedValues['contribution_statuses'][$statusName];
+    }
+
+    throw new CRM_Membershipextrasimporterapi_Exception_InvalidContributionFieldException('Invalid contribution "Status"', 300);
+  }
+
+  private function createFinancialTransactionRecord($mappedContributionParams) {
+    $sqlParams = [
+      1 => [$this->getToFinancialAccountId(), 'Integer'],
+      2 => [$mappedContributionParams['total_amount'], 'Money'],
+      3 => [$mappedContributionParams['currency'], 'String'],
+      4 => [$mappedContributionParams['contribution_status_id'], 'Integer'],
+      5 => [$mappedContributionParams['payment_method_id'], 'Integer'],
+    ];
+    $sqlQuery = "INSERT INTO `civicrm_financial_trxn` (`to_financial_account_id`, `total_amount` , `currency`, `status_id` , `payment_instrument_id`) 
+            VALUES (%1 , %2, %3, %4, %5)";
+    CRM_Core_DAO::executeQuery($sqlQuery, $sqlParams);
+
+    $dao = CRM_Core_DAO::executeQuery('SELECT LAST_INSERT_ID() as id');
+    $dao->fetch();
+    return $dao->id;
+  }
+
+  /**
+   * Gets the "To Financial account id" for the contribution.
+   *
+   * Given that the payment processor is required field
+   * for the import, we are using it to get to
+   * get the financial account value.
+   *
+   * @return id
+   */
+  private function getToFinancialAccountId() {
+    $paymentProcessorId = $this->getPaymentProcessorIdFromRecurContribution();
+    $sqlQuery = "SELECT financial_account_id FROM civicrm_entity_financial_account 
+                   WHERE entity_table = 'civicrm_payment_processor' AND entity_id = {$paymentProcessorId}";
+    $result = CRM_Core_DAO::executeQuery($sqlQuery);
+    $result->fetch();
+    return $result->financial_account_id;
+  }
+
+  private function getPaymentProcessorIdFromRecurContribution() {
+    $sqlQuery = "SELECT payment_processor_id from civicrm_contribution_recur WHERE id = {$this->recurContributionId}";
+    $result = CRM_Core_DAO::executeQuery($sqlQuery);
+    $result->fetch();
+    return $result->payment_processor_id;
+  }
+
+  private function createEntityFinancialTransactionRecord($contributionId, $financialTransactionId, $mappedContributionParams) {
+    $sqlParams = [
+      1 => [$mappedContributionParams['total_amount'], 'String'],
+    ];
+    $sqlQuery = "INSERT INTO `civicrm_entity_financial_trxn` (`entity_table`, `entity_id` , `financial_trxn_id`, `amount`) 
+                VALUES ('civicrm_contribution', {$contributionId}, {$financialTransactionId}, %1)";
+    CRM_Core_DAO::executeQuery($sqlQuery, $sqlParams);
+  }
+
+}
